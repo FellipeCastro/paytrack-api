8960be2c8b7a2433e2b6ebd38ac4f0e4579959f9
Fellipe Castro <fellipecastro27d@gmail.com>
2026-01-05 18:07:21 -0300

    error middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes.js";
+import errorMiddleware from "./middlewaers/errors.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errorMiddleware);
 
 export default app;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import sequelize from "./config/database.js";
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  res.send("API is running");
+// health check route
+routes.get("/health", (req, res) => {
+    sequelize
+        .authenticate()
+        .then(() => {
+            res.status(200).json({
+                status: "UP",
+                message: "Database connection is healthy.",
+            });
+        })
+        .catch((error) => {
+            res.status(500).json({
+                status: "DOWN",
+                message: "Database connection failed.",
+                error: error.message,
+            });
+        });
 });
 
 export default routes;
