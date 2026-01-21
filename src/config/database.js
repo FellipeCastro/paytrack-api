461990bf91c7c4daf5149246263da8f72793265e
Fellipe Castro <fellipecastro27d@gmail.com>
2026-01-21 11:24:27 -0300

    update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,19 +4,13 @@ import "dotenv/config";
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
-    dialectOptions:
-        process.env.NODE_ENV === "production"
-            ? {
-                  ssl: {
-                      require: true,
-                      rejectUnauthorized: false,
-                  },
-              }
-            : null,
-    dialectModule:
-        process.env.NODE_ENV === "production"
-            ? await import("pg").then((mod) => mod.default)
-            : null,
+    dialectOptions: process.env.NODE_ENV === "production" ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    } : null,
+    dialectModule: process.env.NODE_ENV === "production" ? await import("pg").then((mod) => mod.default) : null,
 });
 
 export default sequelize;
